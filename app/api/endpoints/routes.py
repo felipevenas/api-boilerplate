24b4e22ab459a3be73effba9cdbbc7bdefad3fb0
Felipe Venas <felipevenas3003@gmail.com>
2026-01-23 21:46:28 -0300

    Fix: Remoção do prefixo do endpoint

--- app/api/endpoints/routes.py
+++ app/api/endpoints/routes.py
@@ -4,4 +4,4 @@ from app.domain.user.routes import router as user_router
 
 router = APIRouter()
 
-router.include_router(user_router, prefix="/users", tags=["Users"])
+router.include_router(user_router, tags=["Users"])
