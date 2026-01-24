0a9f587ed816566dfc70793da854b0756ff302d9
Felipe Venas <felipevenas3003@gmail.com>
2026-01-23 21:46:04 -0300

    Feature: Novos métodos HTTP do CRUD de usuários

--- app/domain/user/routes.py
+++ app/domain/user/routes.py
@@ -26,7 +26,36 @@ def post(data: UserCreate,
          service: UserService = Depends(get_user_service)):
     """ Cria um novo usuário no banco de dados """
     try:
-        user = service.post(data)
-        return success_response(user, "Novo usuário cadastrado com sucesso!")
+        created_user = service.post(data)
+        return success_response(created_user, "Novo usuário cadastrado com sucesso!")
     except ValueError as e:
         return error_response("Ocorreu um erro ao criar um novo usário!", details=str(e))
+    
+@router.get("/user/{id}", response_model=dict, status_code=status.HTTP_200_OK, summary="Buscar um usuário pelo ID")
+def get_by_id(id: int,
+              service: UserService = Depends(get_user_service)):
+    """ Busca um usuário através do seu ID """
+    user = service.get_by_id(id)
+    if not user:
+        return error_response("Ocorreu um erro ao buscar o usuário!", user)
+    return success_response(user, "Usuário encontrado com sucesso!")
+
+@router.put("/user/{id}", response_model=dict, status_code=status.HTTP_200_OK, summary="Alterar dados de um usuário pelo ID")
+def put(id: int,
+        data: UserUpdate,
+        service: UserService = Depends(get_user_service)):
+    """ Alterar dados de um usuário pelo ID """
+    try:
+        updated_user = service.put(id, data)
+        return success_response(updated_user, "Dados do usuário alterados com sucesso!")
+    except ValueError as e:
+        return error_response("Ocorreu um erro ao alterar os dados do usuário!", details=str(e))
+    
+@router.delete("/user/{id}", response_model=dict, status_code=status.HTTP_200_OK, summary="Deleta um usuário pelo ID")
+def delete(id: int,
+           service: UserService = Depends(get_user_service)):
+    """ Deleta um usuário pelo ID """
+    deleted_user = service.delete(id)
+    if not deleted_user:
+        return error_response("Ocorreu um erro ao deletar o usuário!", deleted_user)
+    return success_response(deleted_user, "Usuário deletado com sucesso!")
